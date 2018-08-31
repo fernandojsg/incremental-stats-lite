93a188998148e1aa13d07c555f00b95c5fae061c
Fernando Serrano <fernandojsg@gmail.com>
2018-08-31 02:30:20 +0200

    Moved to classes

--- index.js
+++ index.js
@@ -1,53 +1,47 @@
 'use strict'
 
-export default function Stats () {
-  this.n = 0
-  this.min = Number.MAX_VALUE
-  this.max = -Number.MAX_VALUE
-  this.sum = 0
-  this.mean = 0
-  Object.defineProperty(this, 'q', {
-    enumerable: false,
-    writable: true,
-    value: 0
-  })
-  Object.defineProperty(this, 'variance', {
-    enumerable: true,
-    get: () => { return this.q / this.n }
-  })
-  Object.defineProperty(this, 'standard_deviation', {
-    enumerable: true,
-    get: () => { return Math.sqrt(this.q / this.n) }
-  })
-}
+export default class PerfStats {
+  constructor() {
+    this.n = 0;
+    this.min = Number.MAX_VALUE;
+    this.max = -Number.MAX_VALUE;
+    this.sum = 0;
+    this.mean = 0;
+    this.q = 0;
+  }
 
-Stats.prototype.update = function update (value) {
-  var num = parseFloat(value)
-  if (isNaN(num)) {
-    // Sorry, no NaNs
-    return
+  get variance() {
+    return this.q / this.n;
   }
-  this.n++
-  this.min = Math.min(this.min, num)
-  this.max = Math.max(this.max, num)
-  this.sum += num
-  var prevMean = this.mean
-  this.mean = this.mean + (num - this.mean) / this.n
-  this.q = this.q + (num - prevMean) * (num - this.mean)
-}
 
-Stats.prototype.getAll = function getAll () {
-  if (this.n === 0) {
-    return null
+  get standard_deviation() {
+    return Math.sqrt(this.q / this.n);
   }
-  var s = {
-    n: this.n,
-    min: this.min,
-    max: this.max,
-    sum: this.sum,
-    mean: this.mean,
-    variance: this.variance,
-    standard_deviation: this.standard_deviation
+
+  update(value) {
+    var num = parseFloat(value);
+    if (isNaN(num)) {
+      // Sorry, no NaNs
+      return;
+    }
+    this.n++;
+    this.min = Math.min(this.min, num);
+    this.max = Math.max(this.max, num);
+    this.sum += num;
+    const prevMean = this.mean;
+    this.mean = this.mean + (num - this.mean) / this.n;
+    this.q = this.q + (num - prevMean) * (num - this.mean);
   }
-  return s
+
+  getAll() {
+    return {
+      n: this.n,
+      min: this.min,
+      max: this.max,
+      sum: this.sum,
+      mean: this.mean,
+      variance: this.variance,
+      standard_deviation: this.standard_deviation
+    };
+  }  
 }
